95fa6a7e52375668be39acf2d2ecc3dadddf07e2
dharmikgheewalaai-glitch <dharmikgheewalaai@gmail.com>
2025-09-24 14:16:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pdfplumber
 from extractor import parse_invoice
 
 st.set_page_config(page_title="Invoice Extractor", layout="wide")
-st.title("📑 PDF Invoice Extractor")
+st.title("Invoice Extractor")
 
 uploaded_file = st.file_uploader("Upload Invoice PDF", type=["pdf"])
 
